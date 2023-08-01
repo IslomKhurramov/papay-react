f6a8f39bf63b1b9ae7b15173e76d85d960adc6c1
unknown <islom.khurramov@gmail.com>
2023-08-02 00:30:05 +0900

    feat: create class component example

---
+++ src/app/screens/testCar.tsx
@@ -0,0 +1,34 @@
+//@ts-nocheck
+import { Component, useState } from "react";
+
+class Car extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      brand: "Ford",
+      model: "Mustang",
+      color: "red",
+      year: 1946,
+    };
+  }
+
+  changeColor = () => {
+    this.setState({ color: "blue", model: "TEsla" });
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>My {this.state.brand}</h1>
+        <p>
+          It is a {this.state.color} - {this.state.model}-from {this.state.year}
+        </p>
+        <button type="button" onClick={this.changeColor}>
+          change color
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Car;
