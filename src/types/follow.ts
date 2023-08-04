afdc0767b63647fda2176bf8c12f8e6267c27638
unknown <islom.khurramov@gmail.com>
2023-08-04 23:12:34 +0900

    feat: develop interfaces of AppRootState & HomePageState

---
+++ src/types/follow.ts
@@ -0,0 +1,5 @@
+export interface MeFollowed {
+  follow_d: string;
+  subscriber_id: string;
+  my_following: boolean;
+}
---
+++ src/types/user.ts
@@ -0,0 +1,43 @@
+import { MeFollowed } from "./follow";
+import { MeLiked } from "./product";
+
+export interface Restaurant {
+  _id: string;
+  mb_nick: string;
+  mb_phone: string;
+  mb_password: string;
+  mb_type: string;
+  mb_status: string;
+  mb_address: string;
+  mb_description: string;
+  mb_image: string;
+  mb_point: number;
+  top: string;
+  mb_views: number;
+  mb_likes: number;
+  mb_follow_cnt: number;
+  mb_subscriber_cnt: number;
+  createdAt: Date;
+  me_liked: MeLiked[]; //Meliked
+}
+
+export interface Member {
+  _id: string;
+  mb_nick: string;
+  mb_phone: string;
+  mb_password: string;
+  mb_type: string;
+  mb_status: string;
+  mb_address?: string;
+  mb_description?: string;
+  mb_image?: string;
+  mb_point?: number;
+  top?: string;
+  mb_views: number;
+  mb_likes: number;
+  mb_follow_cnt: number;
+  mb_subscriber_cnt: number;
+  createdAt: Date;
+  me_liked: MeLiked[]; //Meliked
+  me_followed: MeFollowed[]; //Mefollowed
+}
