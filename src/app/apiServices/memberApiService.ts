83a0df4e7bff07fce1d7a2736380860acb6cae1e
unknown <islom.khurramov@gmail.com>
2023-08-08 03:55:04 +0900

    feat: signup Authentication process

--- src/app/apiServices/memberApiService.ts
+++ src/app/apiServices/memberApiService.ts
@@ -28,6 +28,24 @@ class MemberApiService {
       throw err;
     }
   }
+
+  public async signupRequest(signup_data: any) {
+    try {
+      const result = await axios.post(this.path + "/signup", signup_data, {
+        withCredentials: true,
+      });
+      console.log("STATE:::", result.data.data);
+      assert.ok(result?.data, Definer.general_err1);
+      assert.ok(result?.data?.state != "fail", result?.data?.message);
+
+      const memeber: Member = result.data.data;
+      localStorage.setItem("memebr_data", JSON.stringify(memeber));
+      return memeber;
+    } catch (err: any) {
+      console.log(`ERROR::: signupRequest ${err.message}`);
+      throw err;
+    }
+  }
 }
 
 export default MemberApiService;
--- src/app/components/auth/index.tsx
+++ src/app/components/auth/index.tsx
@@ -74,6 +74,28 @@ export default function AuthenticationModal(props: any) {
     }
   };
 
+  const handleSignUpRequest = async () => {
+    try {
+      const is_fulfilled = mb_nick != "" && mb_password != "" && mb_phone != 0;
+      assert.ok(is_fulfilled, Definer.input_err1);
+
+      const signup_data = {
+        mb_nick: mb_nick,
+        mb_phone: mb_phone,
+        mb_password: mb_password,
+      };
+
+      const memberApiService = new MemberApiService();
+      await memberApiService.signupRequest(signup_data);
+
+      props.handleSignUpClose();
+      window.location.reload;
+    } catch (err) {
+      console.log(err);
+      sweetErrorHandling(err).then;
+    }
+  };
+
   return (
     <div>
       {/*@ts-ignore*/}
@@ -97,27 +119,27 @@ export default function AuthenticationModal(props: any) {
             <Stack sx={{ marginLeft: "69px", alignItems: "center" }}>
               <h2>SignUp Form</h2>
               <TextField
-                // onChange={}
+                onChange={handleUserName}
                 sx={{ marginTop: "7px" }}
                 id="outlined-basic"
                 label="username"
                 variant="outlined"
               />
               <TextField
-                // onChange={}
+                onChange={handlePhone}
                 sx={{ my: "17px" }}
                 id="outlined-basic"
                 label="phone number"
                 variant="outlined"
               />
               <TextField
-                // onChange={}
+                onChange={handlePassword}
                 id="outlined-basic"
                 label="password"
                 variant="outlined"
               />
               <Fab
-                // onClick={}
+                onClick={handleSignUpRequest}
                 sx={{ marginTop: "30px", width: "120px" }}
                 variant="extended"
                 color="primary">
