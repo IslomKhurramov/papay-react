e7e4173ef9979aa0ba336bb888eb9989ae853020
unknown <islom.khurramov@gmail.com>
2023-08-16 02:20:26 +0900

    feat: organize retrieve of MemberPage url query data

--- src/app/screens/MemberPage/index.tsx
+++ src/app/screens/MemberPage/index.tsx
@@ -1,18 +1,31 @@
-import { Route, Switch, useRouteMatch } from "react-router-dom";
+import { Route, Switch, useLocation, useRouteMatch } from "react-router-dom";
 import React from "react";
 import { VisitOtherPage } from "./VisitOtherPage";
 import { VisitMyPage } from "./VisitMayPage";
 import "../../../css/my_page.css";
+import "../../../css/my_page.css";
+
+function useQuery() {
+  const { search } = useLocation();
+  return React.useMemo(() => new URLSearchParams(search), [search]);
+}
 
 export function MemberPage(props: any) {
   const { verifiedMemberData } = props;
   let member = useRouteMatch();
-  // console.log(member);
+  const query = useQuery();
+  const chosen_mb_id: string | null = query.get("mb_id") ?? null;
+  const chosen_art_id: string | null = query.get("art_id") ?? null;
+
   return (
     <div className="member_page">
       <Switch>
         <Route path={`${member.path}/other`}>
-          <VisitOtherPage verifiedMemberData={verifiedMemberData} />
+          <VisitOtherPage
+            verifiedMemberData={verifiedMemberData}
+            chosen_mb_id={chosen_mb_id}
+            chosen_art_id={chosen_art_id}
+          />
         </Route>
 
         <Route path={`${member.path}/`}>
